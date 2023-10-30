4b1b9cf62316cfaefa1047648122f7c3ef0550e3
ReformaMark <mark.reforma0117@gmail.com>
2023-10-30 14:34:57 +0800

    feat: Edit profile (Static)

--- src/app/components/(user)/UserPhoto.tsx
+++ src/app/components/(user)/UserPhoto.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
 import DisplayPhoto from '@/app/(pages)/discussion/images/displayphoto.png'
+import Link from 'next/link'
 
 export default function UserPhoto() {
   return (
-    <button className=' w-[2.5rem]'>
+    <Link href={'/profile'} className=' w-[2.5rem]'>
         <Image
             src={DisplayPhoto}
             alt='User Image'
         />
-    </button>
+    </Link>
   )
 }
