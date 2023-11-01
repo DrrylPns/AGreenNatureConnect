9e2079a4156c4c12c899e32edb5de8988b123395
ReformaMark <mark.reforma0117@gmail.com>
2023-11-01 14:23:14 +0800

    create seed.ts

--- src/app/(user)/discussion/components/CreatePost.tsx
+++ src/app/(user)/discussion/components/CreatePost.tsx
@@ -6,12 +6,11 @@ import {BiImageAdd, BiPaperclip } from "react-icons/bi"
 import { UserAvatar } from '@/app/components/UserAvatar'
 import Link from 'next/link'
 import { Button } from '@/app/components/Ui/Button'
-import Loader, { RotatingLines } from "react-loader-spinner"; 
+import { RotatingLines } from "react-loader-spinner"; 
 
 
 export default function CreatePost() {
     const { data: session, status } = useSession();
-    const [user, setUser] = useState(false)
     return (
         <section className='sm:px-[3%] md:pl-[25%] lg:pr-[25%]'>
             {status === 'loading' ? (
@@ -27,7 +26,7 @@ export default function CreatePost() {
             ):(
             <>
                 {session ? (
-                <Link href={''} className=" flex justify-between items-center gap-5 bg-white rounded-lg drop-shadow-lg w-full px-5 py-5">
+                <Link href={'/discussion/create-post'} className=" flex justify-between items-center gap-5 bg-white rounded-lg drop-shadow-lg w-full px-5 py-5">
                     <Link href={'/profile'} className="w-[2.5rem]">
                         <UserAvatar
                             user={{ name: session?.user.username || null, image: session?.user.image || null }}
@@ -35,7 +34,9 @@ export default function CreatePost() {
                         />
                     </Link>
                     <input type="text" placeholder="Create post" className='px-5 py-2 w-[70%] bg-pale rounded-xl '/>
-                    <Button variant={'green'}>Create</Button>
+                    <Link href={'/discussion/create-post'}>
+                        <Button variant={'green'}>Create</Button>
+                    </Link>
                 
                 </Link>
                 ):(
---
+++ src/app/(user)/discussion/create-post/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CreatePost() {
+  return (
+    <main className='pl-[0] md:pl-[22%] md:pr-[7%] pt-[8rem] bg-[#F0EEF6] md:pt-[6rem] min-h-[100vh] h-full font-poppins'>
+        Create
+    </main>
+  )
+}
