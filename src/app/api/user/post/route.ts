91ac120921fb7dbe0254498df98f1fd49a1731a4
ReformaMark <mark.reforma0117@gmail.com>
2023-11-03 22:00:58 +0800

    Make all route handlers centralize

--- src/app/api/user/post/route.ts
+++ src/app/api/user/post/route.ts
@@ -1,41 +1,10 @@
-import { getAuthSession } from "@/lib/auth";
-import prisma from "@/lib/db/db";
-import { PostSchema } from "@/lib/validations/createPostSchema";
+import prisma from "@/lib/db/db"
 import { NextRequest } from "next/server";
+import { getAuthSession } from "@/lib/auth";
+import { PostSchema } from "@/lib/validations/createPostSchema";
 import { z } from "zod";
 
-export async function POST(req: Request) {
-    try {
-        const session = await getAuthSession()
-
-        if (!session?.user) {
-            return new Response("Unauthorized", { status: 401 })
-        }
-
-        const body = await req.json()
-
-        const { title, content, topicId } = PostSchema.parse(body)
-
-        await prisma.post.create({
-            data: {
-                title,
-                content,
-                authorId: session.user.id,
-                topicId
-            }
-        })
-
-        return new Response('OK')
-
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            return new Response('Invalid POST request data passed', { status: 422 })
-        }
-
-        return new Response('Could not post to community at this time, please try again later', { status: 500 })
-    }
-}
-
+//Getting all post
 export async function GET(req: NextRequest) {
     try {
         const getAllPost = await prisma.post.findMany({
@@ -47,7 +16,6 @@ export async function GET(req: NextRequest) {
                    select:{
                     name:true
                    }
-
                 }
             }
         })
@@ -55,4 +23,39 @@ export async function GET(req: NextRequest) {
     } catch (error) {
         return new Response(JSON.stringify({message: 'Error:', error}))
     }
+}
+
+//Creating new post
+export async function POST(req: Request) {
+    try {
+        const session = await getAuthSession()
+        if (!session?.user) {
+            return new Response("Unauthorized", { status: 401 })
+        }
+        const body = await req.json()
+        const { title, content, topicId } = PostSchema.parse(body)
+        await prisma.post.create({
+            data: {
+                title,
+                content,
+                authorId: session.user.id,
+                topicId
+            }
+        })
+        return new Response('OK')
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return new Response('Invalid POST request data passed', { status: 422 })
+        }
+        return new Response('Could not post to community at this time, please try again later', { status: 500 })
+    }
+}
+
+//Deleteng new Post
+export async function DELETE(req: NextRequest){
+
+}
+
+export async function HEAD(req: NextRequest){
+
 }
