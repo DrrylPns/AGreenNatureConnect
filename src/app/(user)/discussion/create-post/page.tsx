f82469cee256be6883cf37fb92951de1dcf151ac
ReformaMark <mark.reforma0117@gmail.com>
2023-11-04 21:41:51 +0800

    chore: Create Post

---
+++ src/app/(user)/discussion/create-post/page.tsx
@@ -0,0 +1,30 @@
+import Editor from '@/app/components/(user)/Editor'
+import { Button } from '@/app/components/Ui/Button'
+import React from 'react'
+
+
+const page = async () => {
+
+
+    return (
+        <div className="flex flex-col items-start gap-6 pt-[8rem] md:pt-[6rem] px-[25%] pb-20">
+            <div className="w-full text-center border-b border-gray-200 pb-5">
+                <h3 className="font-bold font-livvic text-center text-xl leading-6 text-gray-900">Create Post</h3>
+            </div>
+
+            {/* Editor.JS */}
+            <Editor/>
+
+            <div className='w-full flex justify-end'>
+                <Button
+                    type="submit"
+                    className="w-full"
+                    variant='green'
+                    form='topic-post-form'
+                >Post</Button>
+            </div>
+        </div>
+    )
+}
+
+export default page
--- src/lib/validations/createPostSchema.ts
+++ src/lib/validations/createPostSchema.ts
@@ -4,6 +4,6 @@ export type CreatePostType = z.infer<typeof PostSchema>
 
 export const PostSchema = z.object({
     title: z.string().min(3, { message: "Title must be longer than 3 characters" }).max(128, { message: "Title must be atleast 128 characters" }),
-    topicId: z.string(),
+    topicId: z.string().nonempty({ message: "Topic is required" }),
     content: z.any(),
 })
