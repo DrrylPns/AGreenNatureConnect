9083c25b132ee652c53c8146f821ee2087c86c34
drrylpns <darrylpaniss@gmail.com>
2023-11-08 15:43:41 +0800

    feat: blog endpoint logic

---
+++ src/lib/validations/createBlog.ts
@@ -0,0 +1,8 @@
+import { z } from "zod"
+
+export type CreateBlogType = z.infer<typeof BlogSchema>
+
+export const BlogSchema = z.object({
+    title: z.string().min(3, { message: "Title must be longer than 3 characters" }).max(128, { message: "Title must be atleast 128 characters" }),
+    content: z.any(),
+})
