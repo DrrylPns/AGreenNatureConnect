2e7c8db77b1bce332b0bbd8a1029523993420095
drrylpns <darrylpaniss@gmail.com>
2023-11-18 20:01:12 +0800

    feat: unauthorized middleware in layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,6 +5,7 @@ import Providers from '@/lib/providers/Providers'
 import { Toaster } from '../components/toast/toaster'
 import LoginModal from '../components/modals/LoginModal'
 import RegisterModal from '../components/modals/RegisterModal'
+import { getAuthSession } from '@/lib/auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,12 @@ export default async function RootLayout({
 }: {
     children: React.ReactNode,
 }) {
+    const session = await getAuthSession()
+
+    if (session?.user.role === null) return <div>Loading...</div>
+
+    if (session?.user.role !== "ADMIN") return <div>Unauthorized</div>
+
     return (
         <html lang="en">
             <body className={inter.className}>
@@ -25,7 +32,7 @@ export default async function RootLayout({
                     <LoginModal />
                     <RegisterModal />
                     {/* SideBar Render here */}
-                        {/* links === 
+                    {/* links === 
                             dashboard, 
                             create employee, 
                             inventory system, 
