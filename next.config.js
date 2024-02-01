875695d130f42b5725524a77f1359c867de30da6
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-02-01 12:54:20 +0800

    refactor: remove doubled components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const prod = process.env.NODE_ENV === 'production'
 const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+    dest: 'public',
+    register: true,
+    disable: prod ? false : true,
+    skipWaiting: true
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
--- src/app/components/ThemeToggler.tsx
+++ src/app/components/ThemeToggler.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "../components/Ui/Dropdown-Menu";
 
 export function ThemeToggler() {
   const { setTheme } = useTheme();
