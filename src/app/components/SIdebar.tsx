0c00971e505a78be91b09f75573c0e735841b241
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-02-15 13:56:37 +0800

    update: user sidebar

--- src/app/components/SIdebar.tsx
+++ src/app/components/SIdebar.tsx
@@ -25,7 +25,6 @@ import { MdOutlineArticle } from "react-icons/md";
 import { MdOutlineSlowMotionVideo } from "react-icons/md";
 import { ThemeToggler } from "./ThemeToggler";
 
-
 export default function SIdebar() {
   const { data: session, status } = useSession();
   const loginModal = useLoginModal();
@@ -56,8 +55,9 @@ export default function SIdebar() {
           {/**Home, Communities, Markethub Icons and links */}
           <div className="pt-[5rem] w-full flex justify-around bg-white dark:bg-[#242526] md:hidden">
             <Link
-              className={`link ${pathname === "/discussion" ? "border-b border-yellow-400" : ""
-                } flex justify-center items-center  w-full py-3`}
+              className={`link ${
+                pathname === "/discussion" ? "border-b border-yellow-400" : ""
+              } flex justify-center items-center  w-full py-3`}
               href={"/discussion"}
             >
               <div className="text-icons ">
@@ -72,15 +72,17 @@ export default function SIdebar() {
                 <PiUsersThree />
               </div>
               <div
-                className={`font-poppins text-[1rem] ${isSideBarOpen ? "opacity-100 block" : "opacity-0 hidden"
-                  }`}
+                className={`font-poppins text-[1rem] ${
+                  isSideBarOpen ? "opacity-100 block" : "opacity-0 hidden"
+                }`}
               >
                 <PiCaretDown />
               </div>
             </button>
             <Link
-              className={`link ${pathname === "/markethub" ? "border-b border-yellow-400" : ""
-                } flex justify-center items-center  w-full py-3`}
+              className={`link ${
+                pathname === "/markethub" ? "border-b border-yellow-400" : ""
+              } flex justify-center items-center  w-full py-3`}
               href={"/markethub"}
             >
               <div className="text-icons ">
@@ -123,9 +125,13 @@ export default function SIdebar() {
         <div className="flex flex-col items-start w-full">
           <Link
             href={"/discussion"}
-            className={`link ${pathname === "/discussion" ? "border-l-[3px] border-[#4DE69E] bg-[#baebd4]" : ""
-              } flex items-center gap-4 w-full py-2 ${isSideBarOpen ? "justify-start" : "justify-center"
-              } hover:bg-pale`}
+            className={`link ${
+              pathname === "/discussion"
+                ? "border-l-[3px] border-[#4DE69E] bg-[#baebd4]"
+                : ""
+            } flex items-center gap-4 w-full py-2 ${
+              isSideBarOpen ? "justify-start" : "justify-center"
+            } hover:bg-pale`}
           >
             <div className="text-icons ">
               <BiHome />
@@ -150,8 +156,9 @@ export default function SIdebar() {
           <button
             type="button"
             onClick={toggleDropdown}
-            className={`flex items-center gap-4 w-full py-2 ${isSideBarOpen ? "justify-start" : "justify-center"
-              } hover:bg-pale`}
+            className={`flex items-center gap-4 w-full py-2 ${
+              isSideBarOpen ? "justify-start" : "justify-center"
+            } hover:bg-pale`}
           >
             <div className="text-icons ">
               <LiaBookReaderSolid />
@@ -173,10 +180,11 @@ export default function SIdebar() {
               </motion.p>
             </div>
             <div
-              className={`font-poppins text-[1rem] ${isSideBarOpen
-                ? "opacity-100 block self-end"
-                : "opacity-0 hidden"
-                }`}
+              className={`font-poppins text-[1rem] ${
+                isSideBarOpen
+                  ? "opacity-100 block self-end"
+                  : "opacity-0 hidden"
+              }`}
             >
               <div className="text-icons ">
                 <PiCaretDown />
@@ -195,8 +203,9 @@ export default function SIdebar() {
                   delay: 0.5,
                 }}
                 exit={{ opacity: 0, y: -50 }}
-                className={`${!isSideBarOpen && "hidden"} ${isDropdownrOpen ? "flex" : "hidden"
-                  } flex-col `}
+                className={`${!isSideBarOpen && "hidden"} ${
+                  isDropdownrOpen ? "flex" : "hidden"
+                } flex-col `}
               >
                 <Link
                   href={"/learningMaterials"}
@@ -230,9 +239,13 @@ export default function SIdebar() {
           </AnimatePresence>
           <Link
             href={"/markethub"}
-            className={`link ${pathname === "/markethub" ? "border-l-[3px] border-[#4DE69E] bg-[#baebd4]" : ""
-              } flex items-center gap-4 w-full py-2 ${isSideBarOpen ? "justify-start" : "justify-center"
-              } hover:bg-pale `}
+            className={`link ${
+              pathname === "/markethub"
+                ? "border-l-[3px] border-[#4DE69E] bg-[#baebd4]"
+                : ""
+            } flex items-center gap-4 w-full py-2 ${
+              isSideBarOpen ? "justify-start" : "justify-center"
+            } hover:bg-pale `}
           >
             <div className="text-icons ">
               <BiStore />
@@ -256,8 +269,9 @@ export default function SIdebar() {
           </Link>
           <Link
             href={""}
-            className={`flex items-center gap-4 w-full py-2 ${isSideBarOpen ? "justify-start" : "justify-center"
-              } hover:bg-pale`}
+            className={`flex items-center gap-4 w-full py-2 ${
+              isSideBarOpen ? "justify-start" : "justify-center"
+            } hover:bg-pale`}
           >
             <div className="text-icons ">
               <BiInfoCircle />
@@ -282,8 +296,9 @@ export default function SIdebar() {
           </Link>
           <Link
             href={""}
-            className={`flex items-center gap-4 w-full py-2 ${isSideBarOpen ? "justify-start" : "justify-center"
-              } hover:bg-pale`}
+            className={`flex items-center gap-4 w-full py-2 ${
+              isSideBarOpen ? "justify-start" : "justify-center"
+            } hover:bg-pale`}
           >
             <div className="text-icons ">
               <AiOutlineQuestionCircle />
@@ -308,8 +323,9 @@ export default function SIdebar() {
           </Link>
           <Link
             href={"/termsPolicy"}
-            className={`flex items-center gap-4 w-full py-2 ${isSideBarOpen ? "justify-start" : "justify-center"
-              } hover:bg-pale`}
+            className={`flex items-center gap-4 w-full py-2 ${
+              isSideBarOpen ? "justify-start" : "justify-center"
+            } hover:bg-pale`}
           >
             <div className="text-icons ">
               <AiOutlineFileProtect />
@@ -334,8 +350,9 @@ export default function SIdebar() {
           </Link>
           <Link
             href={"/termsPolicy"}
-            className={`flex items-center gap-4 w-full py-2 ${isSideBarOpen ? "justify-start" : "justify-center"
-              } hover:bg-pale`}
+            className={`flex items-center gap-4 w-full py-2 ${
+              isSideBarOpen ? "justify-start" : "justify-center"
+            } hover:bg-pale`}
           >
             <div className="text-icons ">
               <LuFileSignature />
@@ -358,12 +375,37 @@ export default function SIdebar() {
               </motion.p>
             </div>
           </Link>
+          <div
+            className={`flex items-center gap-4 w-full py-2 cursor-pointer ${
+              isSideBarOpen ? "justify-start" : "justify-center"
+            } hover:bg-pale`}
+          >
+            <ThemeToggler />
+
+            <div className={`${isSideBarOpen ? "block" : "hidden"}`}>
+              <motion.p
+                initial={{ scale: 0 }}
+                animate={{ scale: isSideBarOpen ? 1 : 0 }} // Target values (opacity: 1, translateY: 0)
+                transition={{
+                  type: "tween",
+                  stiffness: 1000,
+                  damping: 20,
+                  duration: 0.6,
+                  delay: 0.5,
+                }}
+                style={{ display: isSideBarOpen ? "block" : "none" }}
+                className={`font-poppins text-[1rem] line-clamp-1`}
+              >
+                Themes
+              </motion.p>
+            </div>
+          </div>
         </div>
-        {status === 'authenticated' ? (
+        {status === "authenticated" ? (
           <>
             <div className="flex mt-auto justify-between gap-2">
               <UserAccountAvatar />
-              {isSideBarOpen &&
+              {isSideBarOpen && (
                 <div className={`${isSideBarOpen ? "block" : "hidden"}`}>
                   <motion.div
                     initial={{ scale: 0 }}
@@ -378,32 +420,35 @@ export default function SIdebar() {
                     style={{ display: isSideBarOpen ? "block" : "none" }}
                     className={`text-sm font-semibold font-poppins`}
                   >
-                    <div>
-                      {session?.user.username}
-                    </div>
+                    <div>{session?.user.username}</div>
                     <div className="text-xs text-gray-400 font-poppins">
                       {session?.user.email}
                     </div>
                   </motion.div>
                 </div>
-              }
+              )}
             </div>
-            {isSideBarOpen &&
-              <button type="button" onClick={() => signOut()} className="w-full border border-black mt-3 flex justify-center items-center text-[1rem]">
+            {isSideBarOpen && (
+              <button
+                type="button"
+                onClick={() => signOut()}
+                className="w-full border border-black mt-3 flex justify-center items-center text-[1rem]"
+              >
                 <RiLogoutBoxLine />
                 <span>Logout</span>
               </button>
-            }
-
-
+            )}
           </>
         ) : (
           <>
-            {isSideBarOpen &&
-              <button onClick={loginModal.onOpen} className='w-full mt-auto text-white py-3 bg-green'>
+            {isSideBarOpen && (
+              <button
+                onClick={loginModal.onOpen}
+                className="w-full mt-auto text-white py-3 bg-green"
+              >
                 Signin
               </button>
-            }
+            )}
           </>
         )}
       </motion.div>
