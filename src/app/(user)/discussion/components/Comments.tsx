5917004453f651d535e581ab082f31a3c846f1b4
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-02-15 14:57:37 +0800

    update: comment textarea dark theme

--- src/app/(user)/discussion/components/Comments.tsx
+++ src/app/(user)/discussion/components/Comments.tsx
@@ -136,7 +136,7 @@ export default function Comments({ posts }: { posts: Post }) {
           <textarea
             id="text"
             {...register("text", { required: "Error." })}
-            className="bg-pale min-w-full p-4 pb-8 resize-none border border-gray-300 rounded-lg"
+            className="bg-pale dark:bg-[#4e4f50]  min-w-full p-4 pb-8 resize-none border border-gray-300 rounded-lg"
             placeholder="Add your comment here"
           ></textarea>
 
