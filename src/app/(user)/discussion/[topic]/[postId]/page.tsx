693a73fb075cd0ef43c3f3f316012c1644c244d9
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-02-16 05:20:51 +0800

    fix:  post max md px

--- src/app/(user)/discussion/[topic]/[postId]/page.tsx
+++ src/app/(user)/discussion/[topic]/[postId]/page.tsx
@@ -65,7 +65,7 @@ const page: FC<Props> = ({ params }) => {
   return (
     <main className="pt-[8rem] md:px-[25%] md:pt-[6rem] px-[3%] pb-20 max-md:dark:bg-[#242526] dark:bg-[#18191A] ">
       {posts ? (
-        <div className="dark:bg-[#242526] px-10 max-md:px-0 max-md:pt-0 py-5 mt-5 rounded-lg shadow-lg ">
+        <div className="dark:bg-[#242526] px-10 max-md:px-3 max-md:pt-0 py-5 mt-5 rounded-lg shadow-lg ">
           <button type="button" onClick={() => router.back()}>
             <FiArrowLeft />
           </button>
