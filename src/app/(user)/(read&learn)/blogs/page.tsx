69fdcd3ed2602f0b70acee3a880e65d195c9c474
DrrylPns <160129800+JohnyO-14@users.noreply.github.com>
2024-02-17 18:40:02 +0800

    Fix: vidtutorial, learningmaterials, blogs, article mobile view

--- src/app/(user)/(read&learn)/blogs/page.tsx
+++ src/app/(user)/(read&learn)/blogs/page.tsx
@@ -11,7 +11,7 @@ async function Blogs({ }: {
   const session = await getAuthSession();
 
   return (
-    <div className='pl-64 py-36 w-full h-screen'>
+    <div className='md:pl-64 px-5 py-36 w-full h-screen'>
       {/* <div className=''> */}
       {/* <InformationImage
                 src='https://utfs.io/f/004ab6ca-9251-4611-a71e-71dd5e0705a7-k9bc7k.png'
@@ -24,7 +24,7 @@ async function Blogs({ }: {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda a eveniet amet fuga? Doloremque quaerat dolorum repellat eligendi architecto laborum numquam aspernatur cumque cupiditate! Dolor laudantium minus cupiditate eaque saepe voluptas iste explicabo debitis repellat, sapiente labore culpa aliquid eius similique praesentium. Alias explicabo voluptatem fuga libero magnam expedita beatae molestiae esse minus suscipit debitis dolorum, labore sunt cumque distinctio impedit consequuntur tempora ullam error! Nam cumque quidem tempora qui, sed eaque iste corporis facere aliquam nulla quo, quas error sequi. Non eius quod, aut consectetur commodi similique in nesciunt molestias dignissimos quisquam ea ipsa veritatis, quasi libero veniam fuga?</p>
       </div>
 
-      <Tabs defaultValue="blog" className="flex flex-col gap-3 mt-7 pr-11">
+      <Tabs defaultValue="blog" className="flex flex-col gap-3 mt-7 pr-11 max-md:hidden">
         <TabsList className='flex gap-3 w-full justify-evenly h-11'>
           <TabsTrigger value="videotutorial" >
             <Link href={"/videotutorial"}>
--- src/app/(user)/(read&learn)/learningMaterials/page.tsx
+++ src/app/(user)/(read&learn)/learningMaterials/page.tsx
@@ -6,7 +6,7 @@ import { ApprovedMaterials } from './_components/ApprovedMaterials'
 
 function LearningMaterials() {
   return (
-    <div className='pl-64 py-36 w-full h-screen'>
+    <div className='md:px-64 px-5 py-36 w-full h-screen'>
       {/* <div className=''> */}
       {/* <InformationImage
                 src='https://utfs.io/f/004ab6ca-9251-4611-a71e-71dd5e0705a7-k9bc7k.png'
@@ -19,7 +19,7 @@ function LearningMaterials() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda a eveniet amet fuga? Doloremque quaerat dolorum repellat eligendi architecto laborum numquam aspernatur cumque cupiditate! Dolor laudantium minus cupiditate eaque saepe voluptas iste explicabo debitis repellat, sapiente labore culpa aliquid eius similique praesentium. Alias explicabo voluptatem fuga libero magnam expedita beatae molestiae esse minus suscipit debitis dolorum, labore sunt cumque distinctio impedit consequuntur tempora ullam error! Nam cumque quidem tempora qui, sed eaque iste corporis facere aliquam nulla quo, quas error sequi. Non eius quod, aut consectetur commodi similique in nesciunt molestias dignissimos quisquam ea ipsa veritatis, quasi libero veniam fuga?</p>
       </div>
 
-      <Tabs defaultValue="learningmats" className="flex gap-3 mt-7 flex-col">
+      <Tabs defaultValue="learningmats" className="flex gap-3 mt-7 flex-col max-md:hidden">
         <TabsList className='flex gap-3 w-full justify-evenly h-11'>
           <TabsTrigger value="videotutorial" >
             <Link href={"/videotutorial"}>
