ea2db81a78896ac733d02604bb2b8e3554e6769a
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-02-29 03:51:07 +0800

    update: user account profile toggler in desktop view

--- src/app/components/MenuItem.tsx
+++ src/app/components/MenuItem.tsx
@@ -1,21 +1,19 @@
-'use client';
+"use client";
 
 interface MenuItemProps {
-    onClick?: () => void;
-    label: string;
+  onClick?: () => void;
+  label: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({
-    onClick,
-    label
-}) => {
-    return (
-        <div
-            onClick={onClick}
-            className="py-3 hover:bg-neutral-100 transition rounded-md px-2">
-            {label}
-        </div>
-    )
-}
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="py-3 hover:bg-neutral-100 dark:hover:text-black transition rounded-md px-2"
+    >
+      {label}
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
