bc6ea39ed9c3615e0f74d9959ad4cf00101ad905
DrrylPns <160129800+JohnyO-14@users.noreply.github.com>
2024-03-03 17:03:41 +0800

    update: terms&policy, order details design

--- src/app/(landing-page)/termsPolicy/page.tsx
+++ src/app/(landing-page)/termsPolicy/page.tsx
@@ -16,8 +16,7 @@ export default function TermsPolicy() {
       <header className='relative px-20 md:pt-32 max-sm:pt-14 max-sm:pl-5 pb-8 flex justify-start bg-gradient-to-r from-[#2D5F4D] to-[#2D5F4D80] border-b-[7px] border-black'>
         <div className='text-center max-sm:mt-9'>
           <h1 className='font-poppins font-bold md:text-[30px] max-sm:text-[5x1]'>AGreen Nature Connect</h1>
-          <h1 className='font-poppins font-bold md:text-[30px] max-sm:text-[5x1]'>Terms & Conditions</h1>
-          <h3 className='font-poppins font-medium md:text-[30px] max-sm:text-[5x1]'>We value your privacy</h3>
+          <h1 className='font-poppins font-bold md:text-[30px] max-sm:text-[5x1] max-sm:pb-6 pb-12'>Terms & Conditions</h1>
         </div>
         <div className='absolute md:top-[12.9rem] lg:top-[11.6rem] max-sm:top-[10.1rem] left-0 w-full'>
           <Image
@@ -26,16 +25,8 @@ export default function TermsPolicy() {
           />
         </div>
       </header>
-      <section className='flex max-sm:flex-wrap max-sm:px-6 md:px-20 md:gap-20 lg:gap-10 justify-between pb-10 items-start'>
-        <div className='flex flex-col md:pr-5  w-[20%] justify-center items-center gap-5 py-10 shrink-0 max-sm:pl-24'>
-          <h3 className='text-center font-poppins font-black md:text-[18px] max-sm:text-[10px] max-sm:pl-32 max-sm:mx-32 max-sm:w-96'>Terms and Policies</h3>
-          <div className='flex md:flex-col max-sm:flex-row max-sm:pl-32 items-center gap-6 '>
-          <button type='button' onClick={showTermsConditions} className='justify-center md:px-5 md:mx-24 md:py-3 md:text-[12px] max-sm:w-36 max-sm:h-10 max-sm:px-7 max-sm:py-1 max-sm:text-[9px] font-poppins font-bold rounded-lg bg-amber w-4/5 hover:bg-orange-200 transition-colors ease-linear duration-500 shadow-sm drop-shadow-sm'>Terms & Conditions</button>
-          <button type='button' onClick={showPrivacyPolicy}   className='justify-center md:px-5 md:mx-24 md:py-3 md:text-[12px] max-sm:w-32 max-sm:h-10 max-sm:px-7 max-sm:py-1 max-sm:text-[9px] font-poppins font-bold rounded-lg bg-amber w-4/5 hover:bg-orange-200 transition-colors ease-linear duration-500 shadow-sm drop-shadow-sm'>Privacy Policy</button>
-          </div>
-        </div>
-        {text === 'Terms&Condition' && (
-          <div className='text-justify font-serif md:text-[15px] max-sm:text-[10px] md:py-20 max-sm:pb-3'>
+
+      <div className='text-justify font-serif md:text-[15px] max-sm:text-[10px] md:py-20 max-sm:py-5  max-sm:px-5 px-40'>
             <p className='font-bold'>TERMS & CONDITIONS</p><br/>
             <p className='font-semibold'>Acceptance of Terms</p>
             <p>By accessing or using Agreen-Connect, you agree to comply with and be bound by these terms and conditions. If you do not agree with any part of these terms, you may not use the website.</p><br/>
@@ -65,28 +56,7 @@ export default function TermsPolicy() {
             
             <p>If you have any questions or concerns regarding these terms, please contact us at ✉️natureagreen@gmail.com</p><br/>
           </div>
-        )}
-        {text === 'PrivacyPolicy' && (
-          <div className='text-justify font-serif md:text-[15px] max-sm:text-[10px] md:py-20 max-sm:pb-3'>
-            <p className='font-bold'>PRIVACY POLICY</p><br/>
-            <p className='font-semibold'>Data Collection:</p>
-            <p>By using our urban farming website, you agree to provide us with certain personal information. This information may include your name, email address, location, and other details relevant to our services.</p><br/>
-            <p className='font-semibold'>Data Use:</p>
-            <p>We use the collected personal information to enhance your experience on our website and to provide you with information and updates related to urban farming. Your data may also be used for analytics and research purposes to improve our services.</p><br/>
-            <p className='font-semibold'>Data Protection:</p>
-            <p>We take data protection seriously. Your personal information is stored securely, and we implement measures to safeguard it from unauthorized access or disclosure.</p><br/>
-            <p className='font-semibold'>Third-Party Sharing:</p>
-            <p>At times, we may share your personal information with trusted third parties, such as payment processors or delivery services, for the purpose of fulfilling our services. Rest assured that we only share the necessary information and require these third parties to maintain strict data protection standards.</p><br/>
-            <p className='font-semibold'>Data Rights:</p>
-            <p>You have the right to access, correct, or request the deletion of your personal information held by us. Please contact us for assistance with these requests.?</p><br/>
-            <p className='font-semibold'>Consent:</p>
-            <p>Your use of our website signifies your consent to the collection and use of your personal information as described in this policy. You can withdraw your consent at any time by contacting us.</p><br/>
-            <p className='font-semibold'>Updates:</p>
-            <p>We may update this Privacy and Personal Information clause periodically. It is your responsibility to check for any revisions.</p><br/>
-            <p >Please review our complete Privacy Policy for a comprehensive understanding of how we handle your personal information and If you have any questions, concerns, or requests related to this Privacy Policy, please contact us at 📞09279657750 or ✉️rodel006.re@gmail.com</p>
-          </div>
-        )}
-      </section>
+      
       <Footer />
     </div>
   )
--- src/app/(markethub)/order-status/[transactionId]/page.tsx
+++ src/app/(markethub)/order-status/[transactionId]/page.tsx
@@ -48,17 +48,21 @@ const page: FC<Props> = async({ params }) => {
         </div>
         <Suspense fallback={<Loading/>}>
             <div>
-                <div className=' py-3 px-3 rounded-2xl mx-72 bg-[#C1C1C1] mt-5'>
+                <div className=' py-3 px-3 rounded-2xl mx-72 bg-[#24643B] mt-5'>
+                <div className='border-2 border-black'>
                     <div><h1 className='bg-[#FAD52F] py-5 text-center font-bold'>Ref No.{transaction?.referenceId}</h1></div>
-                    <div className='bg-[#24643B] px-10 max-md:px-3 max-md:pt-3  py-5  max-md:mx-12 shadow-lg  '>
-                            <h1 className='text-white text-center ml-72 px-2 py-2 bg-[#198f51]'><span className='font-bold'>Date:</span> {getDateFormatted()}</h1>
+                    
+                    <div className='bg-[#ffffff] max-md:px-3 max-md:pt-3 max-md:mx-12 shadow-lg  flex flex-auto'>
+                        <h1 className='text-center py-2 px-3 pr-14'><span className='font-bold'>Barangay:</span> {transaction?.seller.name}</h1>
+                
+                        <h1 className='text-center py-2 px-3 pl-14'><span className='font-bold'>Date:</span> {getDateFormatted()}</h1>
+                    </div>   
+                        <div className='bg-[#ffffff] py-2 pl-3 border-2 border-t-[#3b3b3b]'><h1>Recepient: {transaction?.buyer.name}</h1></div>
+                    <div className='bg-[#ffffff]'>
+                            <h1 className='text-center py-3 bg-[#d1e6d9]'><span className='font-bold'>Status:</span> {transaction?.status}</h1>
+                        <div  className='bg-[#D9D9D9] rounded-full border-2 border-black text-center mt-2 py-2 font-bold mx-40'><h1>Total: {transaction?.amount}</h1></div><br/>
+                        <h1 className=' px-5 text-justify'>"Please claim your purchase on {getDateFormatted()} at Barangay {transaction?.seller.name} "</h1><br/>          
                     </div>
-                        <div><h1 className='text-white py-2 px-2 bg-[#198f51]'><span className='font-bold'>Barangay:</span> {transaction?.seller.name}</h1></div>
-                        <div className='bg-[#D9D9D9] py-2  pl-3'><h1>Recepient: {transaction?.buyer.name}</h1></div>
-                    <div className='bg-[#198f51]'>
-                            <h1 className='text-white text-center py-3 bg-[#287e47]'><span className='font-bold'>Status:</span> {transaction?.status}</h1>
-                        <div  className='bg-[#2C623F] text-white rounded-full text-center mt-2 py-2 font-bold mx-40'><h1>Total: {transaction?.amount}</h1></div><br/>
-                        <h1 className='text-white px-5 text-justify'>"Please claim your purchase on {getDateFormatted()} at Barangay {transaction?.seller.name} "</h1><br/>          
                     </div>
                 </div>
 
