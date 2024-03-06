55ebcee7714b46ebec12ca3c3a815158464b6d8e
ReformaMark <mark.reforma0117@gmail.com>
2024-03-06 08:12:53 +0800

    c

--- src/app/api/markethub/shippingInfo/route.ts
+++ src/app/api/markethub/shippingInfo/route.ts
@@ -2,6 +2,24 @@ import { getAuthSession } from "@/lib/auth"
 import prisma from "@/lib/db/db"
 import { revalidatePath } from "next/cache"
 
+export async function GET(req: Request) {
+    try {
+        const session = await getAuthSession()
+        if (!session?.user) {
+            return new Response("Unauthorized", { status: 401 })
+        }
+        const getShippingInfo = await prisma.shippingInfo.findFirst({
+            where:{
+                userId: session.user.id
+            }
+        })
+
+        return new Response(JSON.stringify(getShippingInfo), {status: 200})
+    } catch (error) {
+        return new Response(JSON.stringify({message: 'Error:', error}))
+    }
+}
+
 export async function POST(req: Request) {
     try {
         const session = await getAuthSession()
