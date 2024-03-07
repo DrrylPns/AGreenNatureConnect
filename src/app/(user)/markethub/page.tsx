e406706a14f6d82df655f9d5b338039c2deaf9a4
ReformaMark <mark.reforma0117@gmail.com>
2024-03-07 17:56:16 +0800

    b

--- src/app/(user)/markethub/page.tsx
+++ src/app/(user)/markethub/page.tsx
@@ -1,8 +1,10 @@
 import React, { } from "react";
 import Carousel from "./components/Carousel";
 import BarangayDropdown from "./components/BarangayDropdown";
+import Logo from "@/app/components/Logo/logo";
 import ProductItem from "./components/Product";
 import SearchBar from "./components/SearchBar";
+import prisma from "@/lib/db/db";
 
 export default function Markethub() {
   
@@ -12,10 +14,9 @@ export default function Markethub() {
         <Carousel />
       </div>
       <div className="my-5 md:flex justify-between items-center">
-        <BarangayDropdown />
         <SearchBar/>
       </div>
-    
+      <ProductItem />
     </div>
   );
 }
