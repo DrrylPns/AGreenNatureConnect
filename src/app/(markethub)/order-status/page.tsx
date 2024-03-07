ee86afe414ccfcc80faba1d7ee8a98a1f031f4cc
ReformaMark <mark.reforma0117@gmail.com>
2024-03-07 12:09:58 +0800

    Fix: order status not displaying in production

--- src/app/(markethub)/order-status/page.tsx
+++ src/app/(markethub)/order-status/page.tsx
@@ -5,60 +5,12 @@ import axios from "axios";
 
 function page() {
 
-    const {data: Pending} = useQuery({
-        queryKey: ['pending'],
-        queryFn: async() =>{
-            const res = await axios.get(`/api/markethub/transaction/pending`)
-
-            return res.data
-        }
-    })
-    const {data: Approved} = useQuery({
-        queryKey: ['pending'],
-        queryFn: async() =>{
-            const res = await axios.get(`/api/markethub/transaction/approved`)
-
-            return res.data
-        }
-    })
-    const {data: Pickup} = useQuery({
-        queryKey: ['pending'],
-        queryFn: async() =>{
-            const res = await axios.get(`/api/markethub/transaction/pickUp`)
-
-            return res.data
-        }
-    })
-    const {data: Cancelled} = useQuery({
-        queryKey: ['pending'],
-        queryFn: async() =>{
-            const res = await axios.get(`/api/markethub/transaction/cancelled`)
-
-            return res.data
-        }
-    })
-    const {data: Completed} = useQuery({
-        queryKey: ['pending'],
-        queryFn: async() =>{
-            const res = await axios.get(`/api/markethub/transaction/completed`)
-
-            return res.data
-        }
-    })
+   
   return (
      <div>
-     {Pending === undefined && Approved === undefined && Pickup === undefined && Completed === undefined && Cancelled === undefined ? (
-        <></>
-     ):(
-        <OrderTab 
-            pending={Pending} 
-            approved={Approved} 
-            pickup={Pickup} 
-            cancelled={Cancelled}
-            completed={Completed}
-        />
-     )}
-        
+    
+        <OrderTab />
+ 
      </div>
   )
 }
