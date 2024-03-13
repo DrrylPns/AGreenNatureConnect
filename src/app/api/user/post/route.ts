4e9790c3b0c661cb258dc5d2debe4c46dea786c9
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-03-13 14:15:04 +0800

    Update route.ts

--- src/app/api/user/post/route.ts
+++ src/app/api/user/post/route.ts
@@ -67,24 +67,6 @@ export async function POST(req: Request) {
 }
 
 // Deleting Post
-export async function DELETE(req: NextRequest){
-    const searchParams = req.nextUrl.searchParams
-    const postId = searchParams.get('postId')
-    console.log(`this is your Id: ${postId}`)
-    try {
-        const deletePost = await prisma.comment.delete({
-            where:{
-                id: postId as string
-            }
-        })
-
-
-        console.log(deletePost)
-        return new Response(JSON.stringify(deletePost), {status: 200})
-    } catch (error) {
-    console.log(error)
-}
-}
 
 
 export async function HEAD(req: NextRequest) {
