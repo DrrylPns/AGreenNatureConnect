b814335ac5526de5802cd94c99a46b098bff55eb
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-03-30 01:52:18 +0800

    update

--- src/app/components/(user)/Navbar.tsx
+++ src/app/components/(user)/Navbar.tsx
@@ -19,7 +19,6 @@ import CartIcon from "./CartIcon";
 import LogoIcon from "/public/logo.png";
 import { UserNotifs } from "@/components/UserNotifs";
 
-
 const Navbar = () => {
   const { data: session, status } = useSession();
   const loginModal = useLoginModal();
@@ -42,22 +41,28 @@ const Navbar = () => {
       ) : (
         <div className="flex items-center text-[1.5rem] gap-3 justify-between">
           {status === "authenticated" ? (
-            <div className="flex items-center justify-evenly gap-5 text-[1.5rem] text-yellow-400 font-bold">
+            <div className="flex items-center justify-evenly gap-3.5 text-[1.5rem] text-yellow-400 font-bold">
               {/**  <Notification />
                             <Settings />*/}
               <div className="max-md:block hidden mt-4">
                 <ThemeToggler1 />
               </div>
+              <div className="hidden max-md:block mt-[6%] text-4xl decoration-8">
+                <UserNotifs />
+              </div>
 
-              <div className="hidden md:block mt-[9%] text-5xl decoration-8">
-                <UserNotifs/>
+              <div className="hidden md:block mt-[9%] text-4xl decoration-8">
+                <UserNotifs />
               </div>
 
               <div className="hidden md:block mt-[10%]">
-                <TooltipProvider >
+                <TooltipProvider>
                   <Tooltip>
                     <TooltipTrigger>
-                      <Link href={"/order-status"} className="hidden md:block text-[1.5rem] md:text-[2rem]">
+                      <Link
+                        href={"/order-status"}
+                        className="hidden md:block text-[1.5rem] md:text-[2rem]"
+                      >
                         <MdOutlineHistory />
                       </Link>
                     </TooltipTrigger>
