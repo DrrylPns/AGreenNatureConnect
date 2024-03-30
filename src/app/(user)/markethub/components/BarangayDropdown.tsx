ff5e2a987bed1e7f7bdfb5068576da6ccee9a6a3
ReformaMark <mark.reforma0117@gmail.com>
2024-03-30 19:54:43 +0800

    Add design to communities btn

--- src/app/(user)/markethub/components/BarangayDropdown.tsx
+++ src/app/(user)/markethub/components/BarangayDropdown.tsx
@@ -7,7 +7,10 @@ import { Community } from '@prisma/client'
 import { BiCaretDown } from 'react-icons/bi'
 import { Button } from '@/components/ui/button'
 import banner from '../../../../../public/images/bannerbg.png'
-
+import Image from 'next/image'
+import Bagbag from '../images/BagbagImage1.jpg'
+import Nova from '../images/NovaProper1.jpg'
+import Silangan from '../images/BagongSilangan1.jpg'
 
 
 function BarangayDropdown() {
@@ -27,15 +30,42 @@ function BarangayDropdown() {
         }
     }
   return (
-    <div className={`transition-all font-light md:tracking-wider bg-cover bg-center duration-500 ease-in w-full grid grid-cols-6 border-2 border-gray-300 drop-shadow-md shadow-inner mt-3 p-2 sm:p-5`}>
+    <div className={`transition-all font-light md:tracking-wider bg-cover bg-center duration-500 ease-in w-full grid grid-cols-6 justify-center border-2 border-gray-300 drop-shadow-md shadow-inner mt-3 p-2 sm:p-5`}>
       
       {communities.length > 0 && communities.map((community: Community) =>(
           <Link 
-            className=' md:h-32 w-10 h-10  md:w-32 sm:h-16 sm:w-16 flex justify-center text-[0.5rem] sm:text-sm md:text-xl hover:bg-green hover:text-white transition-colors ease-in duration-300 scale-105 items-center text-center bg-gray-50 border-gray-200 border rounded-lg' 
+            className='relative border-2 border-gray-400 hover:scale-105 hover:shadow-lg md:h-32 w-10 h-10  md:w-32 sm:h-16 sm:w-16 flex justify-center text-[0.5rem] sm:text-sm md:text-xl  hover:text-yellow-300 transition-colors ease-out duration-500 hover:font-bold items-center text-center bg-gray-50 rounded-lg' 
             href={{ pathname:`/markethub/community/${community.name}`, query:{communityId: community.id}} }>
              
-                {community.name}
-          
+                <h1 className='z-30 shadow-md font-semibold text-yellow-300'>{community.name}</h1>
+                {community.name === "Bagbag" && (
+                  <Image 
+                    src={Bagbag} 
+                    alt='' 
+                    width={100}
+                    height={100}
+                    className='absolute top-0 w-full h-full '
+                  />
+                )}
+                {community.name === "Nova Proper" && (
+                  <Image 
+                    src={Nova} 
+                    alt='' 
+                    width={100}
+                    height={100}
+                    className='absolute top-0 w-full h-full '
+                  />
+                )}
+                {community.name === "Bagong Silangan" && (
+                  <Image 
+                    src={Silangan} 
+                    alt='' 
+                    width={100}
+                    height={100}
+                    className='absolute top-0 w-full h-full '
+                  />
+                )}
+               
           </Link>
       
       ))}
