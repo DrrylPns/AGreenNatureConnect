c700af87841f07e238225d7fcdb2fafbff56f6f1
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2024-03-30 15:25:12 +0800

    fix product images in markethub

--- src/app/(user)/markethub/components/Card.tsx
+++ src/app/(user)/markethub/components/Card.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Image, { StaticImageData } from 'next/image';
+import { AspectRatio } from '@/app/components/Ui/aspect-ratio';
 
 function Card({
     imageUrl,
@@ -8,45 +9,71 @@ function Card({
     barangay,
     lowestPrice,
     highestPrice
-}:{
+}: {
     imageUrl: string,
     productName: string,
-    barangay:string,
+    barangay: string,
     lowestPrice: number,
     highestPrice: number
 }) {
-  return (
-    <div className='bg-slate-100 h-fit dark:text-black hover:shadow-xl transition-all duration-300 ease-in-out pb-3 shadow-md drop-shadow-md rounded-sm w-full border border-gray-300'>
-        {/** */}
-        <div className='bg-gray-50'>
-        <Image 
-            src={imageUrl} 
-            alt={productName}
-            width={100}
-            height={100}
-            className='w-full object-contain max-h-[50%] h-1/2 mb-3 border-b-[2px] border-gray-300' 
-        />
-        </div>
-        {/**Product Name */}
-        <div className='px-3'>
-            <h1 className='font-poppins font-semibold text-xs '>{productName}</h1>
-            {/**Barangay */}
-            <h3 className='text-[0.5rem] sm:text-xs font-poppins font-normal'>{barangay}</h3>
-        </div>
-        {lowestPrice == 0 && highestPrice == 0 ?(
-            <div className='flex justify-between p-3 border-t-2 border-gray  px-3 '>
-                <span className='text-xs sm:text-xs md:text-xs font-poppins'>Price:</span>
-                <span className='text-xs sm:text-xs md:text-xs font-poppins'>Free</span>
+    return (
+        <div className='bg-slate-100 h-fit dark:text-black hover:shadow-xl transition-all duration-300 ease-in-out pb-3 shadow-md drop-shadow-md rounded-sm w-full border border-gray-300'>
+            <AspectRatio ratio={16 / 9} className="bg-muted">
+                <Image
+                    src={imageUrl}
+                    alt="Photo by Drew Beamer"
+                    fill
+                    className="rounded-md object-cover"
+                />
+            </AspectRatio >
+            <div className='px-3 my-2'>
+                <h1 className='font-poppins font-semibold text-xs '>{productName}</h1>
+                {/**Barangay */}
+                <h3 className='text-[0.5rem] sm:text-xs font-poppins font-normal'>{barangay}</h3>
             </div>
-        ):(
-            <div className='flex justify-between items-center border-t-2 border-gray pt-3 px-3'>
-                <span className='text-[0.55rem] sm:text-xs md:text-[0.6rem] font-poppins font-bold'>Price:</span>
-                <span className='text-[0.55rem] sm:text-xs md:text-[0.6rem] font-poppins font-bold border border-black rounded-xl px-2 py-1'>₱ {lowestPrice} - ₱ {highestPrice}</span>
-            </div>
-        )}
-        
-    </div>
-  )
+            {lowestPrice == 0 && highestPrice == 0 ? (
+                <div className='flex justify-between p-3 border-t-2 border-gray  px-3 '>
+                    <span className='text-xs md:text-[12px] font-poppins'>Price:</span>
+                    <span className='text-xs md:text-[12px] font-poppins'>Free</span>
+                </div>
+            ) : (
+                <div className='flex justify-between items-center border-t-2 border-gray pt-3 px-3'>
+                    <span className='text-xs md:text-[12px] font-poppins font-bold'>Price:</span>
+                    <span className='text-xs md:text-[12px] font-poppins font-bold border border-black rounded-xl px-2 py-1'>₱ {lowestPrice} - ₱ {highestPrice}</span>
+                </div>
+            )}
+        </div>
+        // <div className='bg-slate-100 h-fit dark:text-black hover:shadow-xl transition-all duration-300 ease-in-out pb-3 shadow-md drop-shadow-md rounded-sm w-full border border-gray-300'>
+        //     {/** */}
+        //     <div className='bg-gray-50'>
+        //     <Image 
+        //         src={imageUrl} 
+        //         alt={productName}
+        //         width={100}
+        //         height={100}
+        //         className='w-full object-contain max-h-[50%] h-1/2 mb-3 border-b-[2px] border-gray-300' 
+        //     />
+        //     </div>
+        //     {/**Product Name */}
+        //     <div className='px-3'>
+        //         <h1 className='font-poppins font-semibold text-xs '>{productName}</h1>
+        //         {/**Barangay */}
+        //         <h3 className='text-[0.5rem] sm:text-xs font-poppins font-normal'>{barangay}</h3>
+        //     </div>
+        //     {lowestPrice == 0 && highestPrice == 0 ?(
+        //         <div className='flex justify-between p-3 border-t-2 border-gray  px-3 '>
+        //             <span className='text-xs sm:text-xs md:text-xs font-poppins'>Price:</span>
+        //             <span className='text-xs sm:text-xs md:text-xs font-poppins'>Free</span>
+        //         </div>
+        //     ):(
+        //         <div className='flex justify-between items-center border-t-2 border-gray pt-3 px-3'>
+        //             <span className='text-xs md:text-[12px] font-poppins font-bold'>Price:</span>
+        //             <span className='text-xs md:text-[12px] font-poppins font-bold border border-black rounded-xl px-2 py-1'>₱ {lowestPrice} - ₱ {highestPrice}</span>
+        //         </div>
+        //     )}
+
+        // </div>
+    )
 }
 
 export default Card
--- src/app/(user)/markethub/components/ProductTab.tsx
+++ src/app/(user)/markethub/components/ProductTab.tsx
@@ -22,15 +22,15 @@ interface Product {
     updatedAt: Date;
     community: Community;
     communityId: string;
-  }
-  interface Community {
+}
+interface Community {
     id: string;
     name: string;
     createdAt: Date;
     updatedAt: Date;
-  
-  }
-  interface Variants {
+
+}
+interface Variants {
     id: string
     unitOfMeasurement: string;
     variant: number;
@@ -38,146 +38,146 @@ interface Product {
     EstimatedPieces: number;
     createdAt: Date;
     updatedAt: Date;
-  }
+}
 
 function ProductTab({
     allProducts,
     vegetables,
     fruits,
     others,
-}:{
+}: {
     vegetables: Product[],
     fruits: Product[],
     allProducts: Product[],
     others: Product[],
 }) {
     const [selectedIndex, setSelectedIndex] = useState(0)
-  return (
-    <div className='pb-10'>
-        <Tab.Group defaultIndex={0} selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-            <Tab.List className={'w-full flex-row bg-gray-100 dark:text-black'}>
-                <Tab className="w-1/4 border-x-2 border-t-2 border-x-slate-300 text-xs md:text-lg font-poppins font-semibold border-b-[5px] outline-none ui-selected:border-b-green transition-all ease-in-out duration-500 py-2 px-4">
-                    <h1>All</h1>
-                </Tab>
-                <Tab className="w-1/4 border-r-2 border-t-2 border-r-slate-300 text-xs md:text-lg font-poppins font-semibold border-b-[5px] outline-none ui-selected:border-b-green transition-all ease-in-out duration-500 py-2 px-4">
-                    <h1>Fruits</h1>
-                </Tab>
-                <Tab className="w-1/4 border-r-2 border-t-2 border-r-slate-300 text-xs md:text-lg font-poppins font-semibold border-b-[5px] outline-none ui-selected:border-b-green transition-all ease-in-out duration-500 py-2 px-4">
-                    <h1>Vegetables</h1>
-                </Tab>
-                <Tab className="w-1/4 border-r-2 border-t-2 border-r-slate-300 text-xs md:text-lg font-poppins font-semibold border-b-[5px] outline-none ui-selected:border-b-green transition-all ease-in-out duration-500 py-2 px-4">
-                    <h1>Others</h1>
-                </Tab>
-            </Tab.List>
-            <Tab.Panels>
-                <Tab.Panel>
-                    <div className="grid grid-cols-12 border-x-[1px]  border-t-2 p-5 border-gray-300 font-poppins font-medium ">
-                        {allProducts.length > 0 ? allProducts.map((product) => {
-                        const prices = product.variants.map((variant) => variant.price);
-                        const lowestPrice = Math.min(...prices);
-                        const highestPrice = Math.max(...prices);
-                        if (product.variants.length < 1) {
-                            return null
-                        }
+    return (
+        <div className='pb-10 h-full'>
+            <Tab.Group defaultIndex={0} selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+                <Tab.List className={'w-full flex-row bg-gray-100 dark:text-black'}>
+                    <Tab className="w-1/4 border-x-2 border-t-2 border-x-slate-300 text-xs md:text-lg font-poppins font-semibold border-b-[5px] outline-none ui-selected:border-b-green transition-all ease-in-out duration-500 py-2 px-4">
+                        <h1>All</h1>
+                    </Tab>
+                    <Tab className="w-1/4 border-r-2 border-t-2 border-r-slate-300 text-xs md:text-lg font-poppins font-semibold border-b-[5px] outline-none ui-selected:border-b-green transition-all ease-in-out duration-500 py-2 px-4">
+                        <h1>Fruits</h1>
+                    </Tab>
+                    <Tab className="w-1/4 border-r-2 border-t-2 border-r-slate-300 text-xs md:text-lg font-poppins font-semibold border-b-[5px] outline-none ui-selected:border-b-green transition-all ease-in-out duration-500 py-2 px-4">
+                        <h1>Vegetables</h1>
+                    </Tab>
+                    <Tab className="w-1/4 border-r-2 border-t-2 border-r-slate-300 text-xs md:text-lg font-poppins font-semibold border-b-[5px] outline-none ui-selected:border-b-green transition-all ease-in-out duration-500 py-2 px-4">
+                        <h1>Others</h1>
+                    </Tab>
+                </Tab.List>
+                <Tab.Panels>
+                    <Tab.Panel>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 mt-2 font-poppins font-medium">
+                            {allProducts.length > 0 ? allProducts.map((product) => {
+                                const prices = product.variants.map((variant) => variant.price);
+                                const lowestPrice = Math.min(...prices);
+                                const highestPrice = Math.max(...prices);
+                                if (product.variants.length < 1) {
+                                    return null
+                                }
 
-                        if (product.kilograms < 1 && product.grams < 1 && product.pounds < 1 && product.packs < 1 && product.pieces < 1) {
-                            return null
-                        } else {
-                            return (
-                            <div key={product.id} className='col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-2'>
-                                <ProductModal product={product} lowestPrice={lowestPrice} highestPrice={highestPrice}/>
-                            </div>
-                            )
-                        }
-                        }) : (
-                        <div className='col-span-12 flex justify-center w-full h-1/2 text-center'>
-                            <h1 className='text-2xl font-livvic font-semibold text-gray-500'>There are no available fruits right now!</h1>
+                                if (product.kilograms < 1 && product.grams < 1 && product.pounds < 1 && product.packs < 1 && product.pieces < 1) {
+                                    return null
+                                } else {
+                                    return (
+                                        <div key={product.id} className=''>
+                                            <ProductModal product={product} lowestPrice={lowestPrice} highestPrice={highestPrice} />
+                                        </div>
+                                    )
+                                }
+                            }) : (
+                                <div className='col-span-12 flex justify-center items-center w-full h-screen text-center'>
+                                    <h1 className='text-2xl font-livvic font-semibold text-gray-500'>There are no available fruits right now!</h1>
+                                </div>
+                            )}
                         </div>
-                        )}
-                    </div> 
-                </Tab.Panel>
-                <Tab.Panel>
-                    <div className="grid grid-cols-12 border-x-[1px]  border-t-2 p-5 border-gray-300 font-poppins font-medium ">
-                        {fruits.length > 0 ? fruits.map((product) => {
-                        const prices = product.variants.map((variant) => variant.price);
-                        const lowestPrice = Math.min(...prices);
-                        const highestPrice = Math.max(...prices);
-                        if (product.variants.length < 1) {
-                            return null
-                        }
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 mt-2 font-poppins font-medium">
+                            {fruits.length > 0 ? fruits.map((product) => {
+                                const prices = product.variants.map((variant) => variant.price);
+                                const lowestPrice = Math.min(...prices);
+                                const highestPrice = Math.max(...prices);
+                                if (product.variants.length < 1) {
+                                    return null
+                                }
 
-                        if (product.kilograms < 1 && product.grams < 1 && product.pounds < 1 && product.packs < 1 && product.pieces < 1) {
-                            return null
-                        } else {
-                            return (
-                            <div key={product.id} className='col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-2'>
-                                <ProductModal product={product} lowestPrice={lowestPrice} highestPrice={highestPrice}/>
-                            </div>
-                            )
-                        }
-                        }) : (
-                        <div className='col-span-12 flex justify-center w-full h-1/2 text-center'>
-                            <h1 className='text-2xl font-livvic font-semibold text-gray-500'>There are no available fruits right now!</h1>
+                                if (product.kilograms < 1 && product.grams < 1 && product.pounds < 1 && product.packs < 1 && product.pieces < 1) {
+                                    return null
+                                } else {
+                                    return (
+                                        <div key={product.id} className=''>
+                                            <ProductModal product={product} lowestPrice={lowestPrice} highestPrice={highestPrice} />
+                                        </div>
+                                    )
+                                }
+                            }) : (
+                                <div className='col-span-12 flex justify-center items-center w-full h-screen text-center'>
+                                    <h1 className='text-2xl font-livvic font-semibold text-gray-500'>There are no available fruits right now!</h1>
+                                </div>
+                            )}
                         </div>
-                        )}
-                    </div> 
-                </Tab.Panel>
-                <Tab.Panel>
-                    <div className="grid grid-cols-12 border-x-[1px]  border-t-2 p-5 border-gray-300 font-poppins font-medium ">
-                        {vegetables.length > 0 ? vegetables.map((product) => {
-                        const prices = product.variants.map((variant) => variant.price);
-                        const lowestPrice = Math.min(...prices);
-                        const highestPrice = Math.max(...prices);
-                        if (product.variants.length < 1) {
-                            return null
-                        }
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 mt-2 font-poppins font-medium">
+                            {vegetables.length > 0 ? vegetables.map((product) => {
+                                const prices = product.variants.map((variant) => variant.price);
+                                const lowestPrice = Math.min(...prices);
+                                const highestPrice = Math.max(...prices);
+                                if (product.variants.length < 1) {
+                                    return null
+                                }
 
-                        if (product.kilograms <= 0 && product.grams <= 0 && product.pounds <= 0 && product.packs <= 0 && product.pieces === 0) {
-                            return null
-                        } else {
-                            return (
-                            <div key={product.id} className='col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-2'>
-                                <ProductModal product={product} lowestPrice={lowestPrice} highestPrice={highestPrice}/>
-                            </div>
-                            )
-                        }
-                        }) : (
-                        <div className=' col-span-12 flex justify-center w-full h-1/2 text-center'>
-                            <h1 className='text-2xl font-livvic font-semibold text-gray-500'>There are no available vegetables right now!</h1>
+                                if (product.kilograms <= 0 && product.grams <= 0 && product.pounds <= 0 && product.packs <= 0 && product.pieces === 0) {
+                                    return null
+                                } else {
+                                    return (
+                                        <div key={product.id} className=''>
+                                            <ProductModal product={product} lowestPrice={lowestPrice} highestPrice={highestPrice} />
+                                        </div>
+                                    )
+                                }
+                            }) : (
+                                <div className=' col-span-12 flex justify-center items-center w-full h-screen text-center'>
+                                    <h1 className='text-2xl font-livvic font-semibold text-gray-500'>There are no available vegetables right now!</h1>
+                                </div>
+                            )}
                         </div>
-                        )}
-                    </div>
-                </Tab.Panel>
-                <Tab.Panel>
-                    <div className="grid grid-cols-12 border-x-[1px]  border-t-2 p-5 border-gray-300 font-poppins font-medium ">
-                        {others.length > 0 ? others.map((product) => {
-                        const prices = product.variants.map((variant) => variant.price);
-                        const lowestPrice = Math.min(...prices);
-                        const highestPrice = Math.max(...prices);
-                        if (product.variants.length < 1) {
-                            return null
-                        }
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 mt-2 font-poppins font-medium">
+                            {others.length > 0 ? others.map((product) => {
+                                const prices = product.variants.map((variant) => variant.price);
+                                const lowestPrice = Math.min(...prices);
+                                const highestPrice = Math.max(...prices);
+                                if (product.variants.length < 1) {
+                                    return null
+                                }
 
-                        if (product.kilograms === 0 && product.grams === 0 && product.pounds === 0 && product.packs === 0 && product.pieces === 0) {
-                            return null
-                        } else {
-                            return (
-                            <div key={product.id} className='col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-2'>
-                                <ProductModal product={product} lowestPrice={lowestPrice} highestPrice={highestPrice}/>
-                            </div>
-                            )
-                        }
-                        }) : (
-                        <div className='col-span-12 flex justify-center w-full h-1/2 text-center'>
-                            <h1 className='text-2xl font-livvic font-semibold text-gray-500'>There are no available other products right now!</h1>
+                                if (product.kilograms === 0 && product.grams === 0 && product.pounds === 0 && product.packs === 0 && product.pieces === 0) {
+                                    return null
+                                } else {
+                                    return (
+                                        <div key={product.id} className=''>
+                                            <ProductModal product={product} lowestPrice={lowestPrice} highestPrice={highestPrice} />
+                                        </div>
+                                    )
+                                }
+                            }) : (
+                                <div className='col-span-12 flex justify-center items-center w-full h-screen text-center'>
+                                    <h1 className='text-2xl font-livvic font-semibold text-gray-500'>There are no available other products right now!</h1>
+                                </div>
+                            )}
                         </div>
-                        )}
-                    </div>
-                </Tab.Panel>
-            </Tab.Panels>
-        </Tab.Group>
-    </div>
-  )
+                    </Tab.Panel>
+                </Tab.Panels>
+            </Tab.Group>
+        </div>
+    )
 }
 
 export default ProductTab
