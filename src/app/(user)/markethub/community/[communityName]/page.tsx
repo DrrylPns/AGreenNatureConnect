f0842b9649f557d28fc6656c4b20c1bfe70cea96
ReformaMark <mark.reforma0117@gmail.com>
2024-03-31 16:48:26 +0800

    Enchance ui for markethub

--- src/app/(user)/markethub/community/[communityName]/page.tsx
+++ src/app/(user)/markethub/community/[communityName]/page.tsx
@@ -8,6 +8,8 @@ import { BagongSilanganCarousel } from '../../components/BagongSilanganCarousel'
 import { BagbagCarousel } from '../../components/BagbagCarousel';
 import Back from '../../components/Back';
 import ProductsByCommunity from '../../components/ProductsByCommunity';
+import { RatingStars } from '../../components/Rating';
+import { Star } from 'lucide-react';
 
 const page = async ({ 
   params,
@@ -17,20 +19,81 @@ const page = async ({
   searchParams?: { communityId: string};
 }) => {
   const communityName = params.communityName.replace(/%20/g, ' ');
-  
+  const allProducts = await prisma.product.findMany({
+    where:{
+      communityId:{
+        equals: searchParams?.communityId
+      },
+      isFree: {
+          equals: false
+      },
+      status:{
+          equals: "APPROVED"
+      },
+    },
+    include:{
+      community: true,
+      variants: true,
+      reviews: true
+    }
+  })
+
+  let reviewCount = 0;
+  let total = 0;
+  allProducts.forEach((product) => {
+    product.reviews.forEach((review) => {
+      total += review.overAllRating;
+      reviewCount++;
+    });
+  });
+  const ratingsAverage = reviewCount > 0 ? total / reviewCount : 0;
   return (
     <div className='relative '>
-      <Back/>
-      <h1 className='text-2xl text-center font-poppins font-bold'>{communityName} Urban Farm</h1>
-      <div className='w-full my-5'>
+      <div className='relative flex items-center justify-center'>
+        <div className='absolute top-2 left-0'>
+          <Back/>
+        </div>
+        <h1 className='text-2xl text-center font-poppins font-bold'>{communityName} Urban Farm</h1>
+      </div>
+      <div className='w-full my-5 bg-gray-50 pb-5 px-5 shadow-sm drop-shadow-md'>
         {communityName === 'Bagbag' && (
-          <BagbagCarousel/>
+          <>
+            <BagbagCarousel/>
+            <div className='font-livvic'>
+              <h1 className='text-lg  text-center tracking-widest font-semibold'>Solo Parent Urban Farming</h1>
+              <p className='text-sm text-gray-500 line-clamp-4'>Solo Parent Urban Farming, the name of community farm of Barangay Bagbag. It is currently manage by Mr. Rodel Edroso, a focal person of Solo Parent Urban Farming. Urban Farming in Bagbag is under the capable management of Mr. Rodel Edroso, who serves as the focal point for Solo Parent Urban Farming. In collaboration with the Department of Agrarian Reform (DAR), the Quezon City government has set a visionary goal for the seven-hectare plot in Barangay Bagbag. They aim to transform it into the city's primary vegetable hub, through an initiative known as "The Seed of Joy of Urban Farming." This project seeks to underscore the importance of urban agriculture, aiming to alleviate poverty, combat hunger, and enhance food security in marginalized urban communities.</p>
+            </div>
+            <div className='flex justify-center gap-10 border-t-2 border-gray-100 pt-5 font-poppins text-xs items-center'>
+              <h1><span className='text-yellow-500 font-semibold'>{allProducts.length}</span> Products</h1>
+              <h1 className='flex items-center'><Star size={10} color='#F7C35F' fill='#F7C35F' className='mr-2'/>Ratings:{ratingsAverage.toFixed(1)} / 5.0</h1>
+            </div>
+          </>
         )}
         {communityName === 'Nova Proper' && (
-          <NovaProperCarousel/>
+          <div className=''>
+            <NovaProperCarousel/>
+            <div className='font-livvic'>
+              <h1 className='text-lg text-center tracking-widest font-semibold'>Sharon Urban Farming</h1>
+              <p className='text-sm text-gray-500 line-clamp-4'>Sharon Urban Farming, the name of community farm of barangay Nova Proper. it is currently manage by Enrique P. Añonuevo, a BESWMC Head of Sharon Urban Farming. Sharon Farm, an integrated urban farm situated at the heart of the city, was launched last June 2, 2021, with collaborative efforts and partnership between public and private stakeholders. The area owned by the Diocese of Novaliches that is approximately 5,500 square meter before the Gulayan sa Siyudad project. This program and movement aim to improve the access of urban settlers and disadvantaged groups to safe and nutritious food, promote social inclusion, and become an important means for recreation and educational activities for the community.</p>
+            </div>
+            <div className='flex justify-center gap-10 border-t-2 pt-5  border-gray-100 font-poppins text-xs items-center'>
+              <h1><span className='text-yellow-500 font-semibold '>{allProducts.length}</span> Products</h1>
+              <h1 className='flex items-center'><Star size={10} color='#F7C35F' fill='#F7C35F' className='mr-2'/>Ratings:{ratingsAverage.toFixed(1)} / 5.0</h1>
+            </div>
+          </div>
         )}
         {communityName === 'Bagong Silangan' && (
-          <BagongSilanganCarousel/>
+          <>
+             <BagongSilanganCarousel/>
+            <div className='font-livvic'>
+              <h1 className='text-lg  text-center tracking-widest font-semibold'>New Greenland Farming</h1>
+              <p className='text-sm text-gray-500 line-clamp-4'>New Greenland Farm, the name of community farm of barangay Barangay Bagong Silangan Quezon City. It is the first urban vegetable farm under the Buhay sa Gulay initiative of the Department of Agrarian Reform, Department of Agriculture, and the Quezon City Government. The farm aims to promote urban farming and yielded 700 kilos of veggies in its first harvest.</p>
+            </div>
+            <div className='flex justify-center gap-10 border-t-2 pt-5  border-gray-100 font-poppins text-xs items-center'>
+              <h1><span className='text-yellow-500 font-semibold'>{allProducts.length}</span> Products</h1>
+              <h1 className='flex items-center'><Star size={10} color='#F7C35F' fill='#F7C35F' className='mr-2'/>Ratings:{ratingsAverage.toFixed(1)} / 5.0</h1>
+            </div>
+          </>
         )}
       </div>
       <div className='border-2 border-gray-300 px-3 sm:px-5 pt-5 min-h-40 rounded-md bg-gray- shadow-lg drop-shadow-sm '>
--- src/app/(user)/markethub/components/BarangayDropdown.tsx
+++ src/app/(user)/markethub/components/BarangayDropdown.tsx
@@ -8,9 +8,9 @@ import { BiCaretDown } from 'react-icons/bi'
 import { Button } from '@/components/ui/button'
 import banner from '../../../../../public/images/bannerbg.png'
 import Image from 'next/image'
-import Bagbag from '../images/BagbagImage1.jpg'
-import Nova from '../images/NovaProper1.jpg'
-import Silangan from '../images/BagongSilangan1.jpg'
+import Sharon from "@/../public/images/Sharon.png";
+import SoloParent from "@/../public/images/SoloParent.png";
+import Greenland from "@/../public/images/Greenland.png";
 
 
 function BarangayDropdown() {
@@ -30,41 +30,51 @@ function BarangayDropdown() {
         }
     }
   return (
-    <div className={`transition-all font-light md:tracking-wider bg-cover bg-center duration-500 ease-in w-full grid grid-cols-6 justify-center border-2 border-gray-300 drop-shadow-md shadow-inner mt-3 p-2 sm:p-5`}>
+    <div className={`transition-all font-light md:tracking-wider bg-cover bg-center duration-500 ease-in w-full grid grid-cols-3 gap-5 md:grid-cols-6 justify-center border-2 border-gray-300 drop-shadow-md shadow-inner mt-3 p-2 sm:p-5`}>
       
       {communities.length > 0 && communities.map((community: Community) =>(
           <Link 
-            className='relative border-2 border-gray-400 hover:scale-105 hover:shadow-lg md:h-32 w-10 h-10  md:w-32 sm:h-16 sm:w-16 flex justify-center text-[0.5rem] sm:text-sm md:text-xl  hover:text-yellow-300 transition-colors ease-out duration-500 hover:font-bold items-center text-center bg-gray-50 rounded-lg' 
+            className='relative border-2 border-gray-400 hover:scale-105 hover:shadow-lg md:h-32   md:w-32 sm:h-16 sm:w-16 flex flex-col justify-center text-[0.5rem] sm:text-sm md:text-xl  hover:text-yellow-300 transition-colors ease-out duration-500 hover:font-bold items-center text-center bg-gray-50 rounded-lg' 
             href={{ pathname:`/markethub/community/${community.name}`, query:{communityId: community.id}} }>
              
-                <h1 className='z-30 shadow-md font-semibold text-yellow-300'>{community.name}</h1>
+              
                 {community.name === "Bagbag" && (
+                  <>
                   <Image 
-                    src={Bagbag} 
+                    src={SoloParent} 
                     alt='' 
                     width={100}
                     height={100}
-                    className='absolute top-0 w-full h-full '
+                    className='w-full h-full '
                   />
+                  <h1 className='z-30 text-xs shadow-md font-semibold '>{community.name}</h1>
+                  </>
                 )}
                 {community.name === "Nova Proper" && (
+                  <>
                   <Image 
-                    src={Nova} 
+                    src={Sharon} 
                     alt='' 
                     width={100}
                     height={100}
-                    className='absolute top-0 w-full h-full '
+                    className='w-full h-full '
                   />
+                  <h1 className='z-30 text-xs shadow-md font-semibold '>{community.name}</h1>
+                  </>
                 )}
                 {community.name === "Bagong Silangan" && (
+                  <>
                   <Image 
-                    src={Silangan} 
+                    src={Greenland} 
                     alt='' 
                     width={100}
                     height={100}
-                    className='absolute top-0 w-full h-full '
+                    className='w-full h-full '
                   />
+                  <h1 className='z-30 text-xs shadow-md font-semibold '>{community.name}</h1>
+                  </>
                 )}
+                 
                
           </Link>
       
