c5afc90e47bf5d2829e943d945b1b7446264e3af
ReformaMark <mark.reforma0117@gmail.com>
2024-05-05 17:37:47 +0800

    add create activity history to product and transaction actions

--- actions/transaction.ts
+++ actions/transaction.ts
@@ -9,14 +9,20 @@ export const insertProof = async (img: string, id: string) => {
     if (!id) return { error: "No transaction id found." }
 
     if (!img) return { error: "No image found." }
+    const session = await getAuthSession()
 
-    await prisma.transaction.update({
+    if (!session) return { error: "Unauthorized" }
+
+    const transaction = await prisma.transaction.update({
         where: { id },
+        include:{
+            buyer: true
+        },
         data: {
             gcashReciept: img
         },
     })
-
+   
     revalidatePath("/order-status")
     revalidatePath("/orders")
     return { success: "Successfully uploaded proof." }
@@ -35,12 +41,27 @@ export const confirmProof = async (id: string) => {
 
     if (user.role === "USER") return { error: "Invalid action!" }
 
-    await prisma.transaction.update({
+    const transaction = await prisma.transaction.update({
         where: { id },
+        include:{
+            buyer: true
+        },
         data: {
             paymentStatus: "Paid"
         },
     })
+    await prisma.employeeActivityHistory.create({
+        data:{
+          type: "MARKETHUB_ORDERS",
+          transactionId: transaction.id,
+          employeeId: session.user.id,
+          amount: transaction.amount,
+          buyer: transaction.buyer.name + " " + transaction.buyer.lastName,
+          paymentStatus: transaction.paymentStatus,
+          status: transaction.status,
+          typeOfActivity: "Approved the Order"
+        }
+      })
 
     revalidatePath("/orders")
     return { success: "Payment status confirmed." }
