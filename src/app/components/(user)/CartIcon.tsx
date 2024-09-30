cd63f6826c00ee89e77d4cfa25bf7abc2f135b56
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2024-10-01 05:58:56 +0800

    refactor: cart functionality

--- src/app/components/(user)/CartIcon.tsx
+++ src/app/components/(user)/CartIcon.tsx
@@ -7,29 +7,35 @@ import axios from 'axios';
 import { useCart } from '@/contexts/CartContext';
 
 function CartIcon() {
-  const { cartNumber, setCartNumber} = useCart();
+  const { cartNumber, setCartNumber } = useCart();
   const loginModal = useLoginModal();
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     getCartCount()
-  },[cartNumber])
+  }, [cartNumber])
 
   const getCartCount = async () => {
     try {
-      const cartCount =  (await axios.get(`/api/markethub/cart/cartNumber`)).data
+      const cartCount = (await axios.get(`/api/markethub/cart/cartNumber`)).data
       setCartNumber(cartCount)
     } catch (error) {
-      
+
     }
-    
+
   }
   return (
     <div className='relative'>
-      <div className='absolute top-[-10px] right-[-3px] text-[0.6rem] sm:text-xs px-2 py-1 bg-red-600 rounded-full text-white'>
+      {/* if cart number is 0 then dont show this div */}
+      {cartNumber > 0 && (
+        <div className='absolute top-[-10px] right-[-3px] text-[0.6rem] sm:text-xs px-2 py-1 bg-red-600 rounded-full text-white'>
+          <h1 className=''>{cartNumber > 99 ? "99+" : cartNumber}</h1>
+        </div>
+      )}
+      {/* <div className='absolute top-[-10px] right-[-3px] text-[0.6rem] sm:text-xs px-2 py-1 bg-red-600 rounded-full text-white'>
         <h1 className=''>{cartNumber > 99 ? "99+" : cartNumber}</h1>
-      </div>
+      </div> */}
       <BsCart4 />
-   
+
     </div>
 
   )
